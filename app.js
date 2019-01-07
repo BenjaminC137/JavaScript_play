32bc620a9590c13765571851e4ed9ef30ccf483f
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2019-01-07 17:58:02 -0600

    pexelsAPI with headers from postman

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //var slideButton = document.getElementById('slider');
 //var slideLeft = window.getComputedStyle(slideButton).left;
+var	pexelsAPI;
+
 var slideState = true;
 function Slide(){
 //	slideButton.style.left = "20px";
@@ -14,4 +16,23 @@ slideState = !slideState;
 		$('input')[0].value = 'go left';
 
 	}
+}
+
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.pexels.com/v1/curated?per_page=15&page=1",
+  "method": "GET",
+  "headers": {
+    "Authorization": "563492ad6f91700001000001d287cf6b84eb4d789e2a5b915da35960",
+    "Cache-Control": "no-cache",
+    "Postman-Token": "334da920-22c4-4c9c-9de9-f721790e32fd"
+  }
+}
+function ChangeBackground(){
+	pexelAPI = '563492ad6f91700001000001d287cf6b84eb4d789e2a5b915da35960';
+	$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 }
---
+++ weather.js
@@ -0,0 +1,57 @@
+//var cityAndState = "Chicago, IL";
+
+var forcastResult, jsonDataTenDay, resultsText;
+
+var locationSubmission;
+
+var weatherAPI;
+
+var cityEntry;
+var stateEntry;
+
+$("weather").toggle();
+$("place").toggle();
+
+
+function getFormvalue(){
+	cityEntry = document.getElementsByName("cityEntry")[0];
+	stateEntry = document.getElementsByName("stateEntry")[0];
+
+	showLocalWeather();
+	return false;
+}
+
+function showLocalWeather(){
+		$("weather").fadeOut(1000);
+		$("place").fadeOut(1000);
+
+		locationSubmission = (cityEntry.value + "," + stateEntry.value);
+		console.log(locationSubmission);
+
+	weatherAPI = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + locationSubmission + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	console.log(weatherAPI);
+
+	$.getJSON(weatherAPI, (jsonData) => {
+
+		$("place").html(jsonData.query.results.channel.description); })
+		$("place").fadeIn(1000);
+
+	$.getJSON(weatherAPI, (jsonData) => {
+		$("weather").html(jsonData.query.results.channel.item.description);
+
+		var textMod = $("weather").html()
+		textMod = textMod.replace("http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*","");
+
+		textMod = textMod.replace("]]&gt;","");
+		textMod = textMod.replace(">Full","target=\"_blank\" >Full");
+
+		$("weather").html(textMod);
+		$("weather").fadeIn(1000);
+	});
+
+
+
+}
+
+
+//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
