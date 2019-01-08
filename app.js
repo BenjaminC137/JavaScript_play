292bb56e68d3ba785255c91ec9da689f1e26bfdf
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2019-01-08 16:00:39 -0600

    working navigation buttons

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
 //var slideButton = document.getElementById('slider');
 //var slideLeft = window.getComputedStyle(slideButton).left;
 var	pexelsAPI;
-
-var slideState = true;
 function Slide(direction){
-slideState = !slideState;
-	var positionX = $('#slider')[0].
+
+//	console.log(direction);
+	var slider = document.querySelector('#slider');
+//	console.log(slider);
+	var sliderLeft = window.getComputedStyle(slider).left;
+	var sliderTop = window.getComputedStyle(slider).top;
+//	console.log(sliderLeft);
+	positionX = Number(sliderLeft.replace("px", ""));
+	console.log(positionX);
+	positionY = Number(sliderTop.replace("px", ""));
+
+	if(positionX < 100){ // if hit left
+		positionX = 101;
+		positionX += "px"
+		slider.style.left = positionX;
+	}
+
+	if(positionX > window.innerWidth - 100){ // if hit right
+		positionX = window.innerWidth - 105;
+		positionX += "px"
+		slider.style.left = positionX;
+	}
+
+	if(positionY < 100){ // if hit top
+		positionY = 101;
+		positionY += "px"
+		slider.style.top = positionY;
+	}
+
+	if(positionY > window.innerHeight - 170){ // if hit bottom
+		positionY = window.innerHeight - 176;
+		positionY += "px"
+		slider.style.top = positionY;
+	}
+
+	if(direction == 'r'){
+		positionX = positionX + 100;
+		positionX += "px";
+		console.log(positionX);
+		slider.style.left = positionX;
+	}
+	if(direction == 'd'){
+		positionY = positionY + 100;
+		positionY += "px";
+		console.log(positionY);
+		slider.style.top = positionY;
+	}
+	if(direction == 'l'){
+		positionX = positionX - 100;
+		positionX += "px";
+		console.log(positionX);
+		slider.style.left = positionX;
+	}
+	if(direction == 'u'){
+		positionY = positionY - 100;
+		positionY += "px";
+		console.log(positionY);
+		slider.style.top = positionY;
+	}
+
 //	if(slideState == false){
 //		$('#slider')[0].style.left = '25px';
 //		$('div')[1].value = 'go right';
@@ -16,7 +72,6 @@ slideState = !slideState;
 //	}
 
 }
-
 
 var settings = {
   "async": true,
@@ -29,6 +84,7 @@ var settings = {
     "Postman-Token": "334da920-22c4-4c9c-9de9-f721790e32fd"
   }
 }
+
 function ChangeBackground(){
 	pexelAPI = '563492ad6f91700001000001d287cf6b84eb4d789e2a5b915da35960';
 	$.ajax(settings).done(function (response) {
